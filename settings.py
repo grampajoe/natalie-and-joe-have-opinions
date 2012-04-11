c52b1bd25e51fa0c839364d9da0ee9c39c4facb3
Joe Friedl <joe@joefriedl.net>
2012-04-11 16:30:11 -0400

    Media url

--- settings.py
+++ settings.py
@@ -52,7 +52,7 @@ MEDIA_ROOT = PROJECT_ROOT + '/media/'
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = 'http://static.natalieandjoe.com/'
+MEDIA_URL = 'http://media.natalieandjoe.com/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
